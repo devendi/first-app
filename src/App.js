6b0b1d4b42b09c2af9f3a189ce621693ba088238
devendi <endi.efendi@gmail.com>
2022-05-19 01:51:35 +0700

    learn state teknik 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 
 class App extends Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+      name : "Parid"
+    }
+  }
   render (){
     return (
       <div>
-        <div>Hello World</div>
-        <div>Hello World</div>
+        <div>Hello {this.state.name}</div>
       </div>
     )
   }
