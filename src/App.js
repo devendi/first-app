155cfa8880289634284111048c4c24c7d6fd85ae
devendi <endi.efendi@gmail.com>
2022-05-19 01:46:45 +0700

    hello world --=> twice line

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 class App extends Component {
   render (){
-    return <div>Hello World</div>
+    return (
+      <div>
+        <div>Hello World</div>
+        <div>Hello World</div>
+      </div>
+    )
   }
 }
 
